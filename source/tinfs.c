ebb47ba9ea6f4b31f76b969dcc0c3945cb5e9785
Pika <15848969+ThatNerdyPikachu@users.noreply.github.com>
2018-10-17 20:12:50 -0400

    nuke /tinfoil/ticket creation

--- source/tinfs.c
+++ source/tinfs.c
@@ -41,14 +41,6 @@ Result createTinfoilDirs(void)
 
     rc = 0;
 
-    if (R_FAILED(rc = fsFsCreateDirectory(&sdFs, "/tinfoil/ticket")) && rc != 0x402)
-    {
-        printf("createTinfoilDirs: Failed to create /tinfoil/ticket. Error code: 0x%08x\n", rc);
-        goto CLEANUP;
-    }
-
-    rc = 0;
-
     CLEANUP:
     fsFsClose(&sdFs);
     return rc;
