f02b8ccc5e318abce585f2eb6c1e0590cacdc566
XorTroll <33005497+XorTroll@users.noreply.github.com>
2018-11-10 17:16:16 +0100

    Moved old tinfoil to folder

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -1,22 +1,6 @@
 #include <gleaf/gleaf.hpp>
 
-// The UI is still not finished, it is not even here.
-// Latest test searched for "sd:/Test.nsp" and installed it, but installation still fails?...
-// Go ahead, but I don't recommend building this project.
-
 int main()
 {
-    consoleInit(NULL);
-    gleaf::Initialize();
-    gleaf::nsp::Installer inst(gleaf::Destination::SdCard, "@Sdcard://Test.nsp", true);
-    inst.InitializeRecords();
-    inst.InstallNCAs([](gleaf::ncm::ContentRecord record, u32 A, u32 B, int C)
-    {
-        std::cout << A << " - " << B << " - " << C << std::endl;
-        consoleUpdate(NULL);
-    });
-    inst.Finish();
-    consoleExit(NULL);
-    gleaf::Finalize();
     return 0;
 }
