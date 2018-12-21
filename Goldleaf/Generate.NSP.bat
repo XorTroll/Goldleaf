344e7e17fb21ab9267a14a148f23e341dbef5d3b
Pika <15848969+ThatNerdyPikachu@users.noreply.github.com>
2018-12-21 10:29:13 -0500

    Update Generate.NSP.bat

--- Goldleaf/Generate.NSP.bat
+++ Goldleaf/Generate.NSP.bat
@@ -2,6 +2,10 @@
 
 rem IMPORTANT! Place keys.dat at "BuildTools/keys.dat"!
 
+del ExeFs\main
+make
+copy "Output\Goldleaf.nso" "ExeFs\main"
+
 del Output\050032a5cf12e000.nsp
 BuildTools\hacbrewpack.exe -k BuildTools\keys.dat --exefsdir=ExeFs --romfsdir=RomFs --nologo --controldir=Control --nspdir=Output
 ren Output\050032a5cf12e000.nsp Goldleaf.nsp
