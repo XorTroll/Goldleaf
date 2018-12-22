535d9fc22efd46fa9ccf3abd1626294400fe6d4b
XorTroll <xabofdez@outlook.es>
2018-12-22 15:56:50 +0100

    Fix control NACP, now allows screenshots

--- Goldleaf/Generate.sh
+++ Goldleaf/Generate.sh
@@ -1,10 +1,11 @@
-echo IMPORTANT! Place keys.dat at "BuildTools/keys.dat"!
 
-rm -r ExeFs/main
+# IMPORTANT! Place keys.dat at "BuildTools/keys.dat"!
+
+rm -r ExeFs/main &> /dev/null
 make
 cp Output/Goldleaf.nso ExeFs/main
-rm Output/Goldleaf.nso
-rm Output/Goldleaf.nsp
-cd BuildTools
-./hacbrewpack -k BuildTools/keys.dat --exefsdir=ExeFs --romfsdir=RomFs --nologo --controldir=Control --nspdir=Output
+rm Output/Goldleaf.nso &> /dev/null
+rm Output/Goldleaf.nsp &> /dev/null
+./BuildTools/hacbrewpack -k BuildTools/keys.dat --exefsdir=ExeFs --romfsdir=RomFs --nologo --controldir=Control --nspdir=Output
 mv Output/050032a5cf12e000.nsp Output/Goldleaf.nsp
+read -n1 -r -p "Press any key to continue..." key
