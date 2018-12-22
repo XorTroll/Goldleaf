b591c329277f1290f871dac9b081fbacefc463ef
Pika <15848969+ThatNerdyPikachu@users.noreply.github.com>
2018-12-22 14:33:18 -0500

    improve unix build script
    1. rm allows multiple params
    2. rm doesn't show any output unless an error occured

--- Goldleaf/Generate.sh
+++ Goldleaf/Generate.sh
@@ -1,11 +1,9 @@
-
 # IMPORTANT! Place keys.dat at "BuildTools/keys.dat"!
 
 rm -r ExeFs/main &> /dev/null
 make
 cp Output/Goldleaf.nso ExeFs/main
-rm Output/Goldleaf.nso &> /dev/null
-rm Output/Goldleaf.nsp &> /dev/null
+rm Output/Goldleaf.nso Output/Goldleaf.nsp
 ./BuildTools/hacbrewpack -k BuildTools/keys.dat --exefsdir=ExeFs --romfsdir=RomFs --nologo --controldir=Control --nspdir=Output
 mv Output/050032a5cf12e000.nsp Output/Goldleaf.nsp
 read -n1 -r -p "Press any key to continue..." key
