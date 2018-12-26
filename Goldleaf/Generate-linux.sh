8bb14f40711fd68cd4b0310795d14fd978655ed1
zkitx <ekosater@gmail.com>
2018-12-26 15:59:21 -0500

    linux nsp building support

---
+++ Goldleaf/Generate-linux.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# IMPORTANT! Place keys.dat at "BuildTools/keys.dat"!
+
+rm -r ExeFs/main &> /dev/null
+make
+cp Output/Goldleaf.nso ExeFs/main
+rm Output/Goldleaf.nso Output/Goldleaf.nsp
+./BuildTools/hacbrewpack-linux -k BuildTools/keys.dat --exefsdir=ExeFs --romfsdir=RomFs --nologo --controldir=Control --nspdir=Output
+mv Output/050032a5cf12e000.nsp Output/Goldleaf.nsp
+read -n 1 -s -r -p "Press any key to continue..."$'\n'
