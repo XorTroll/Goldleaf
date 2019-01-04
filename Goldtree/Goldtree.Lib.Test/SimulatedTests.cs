9b54c5414d1b668e11bbb64222ebae4e7d91016d
2767mr <2767mr@gmx.at>
2019-01-04 23:13:37 +0100

    Added a unit test

--- Goldtree/Goldtree.Lib.Test/SimulatedTests.cs
+++ Goldtree/Goldtree.Lib.Test/SimulatedTests.cs
@@ -19,5 +19,20 @@ namespace Goldtree.Lib.Test
             bool result = cli.SendFile(TestUtils.GetResource("empty.nsp"));
             Assert.IsFalse(result, "Sending an empty(/invalid) nsp must not succeed");
         }
+
+        [TestMethod]
+        public void EmptyTxtTest()
+        {
+            CLI.IsArgumentsOnly = true;
+            VirtualUsb usb = new VirtualUsb(
+                new Command(CommandId.ConnectionResponse),
+                new Command(CommandId.Start)
+                );
+            CLI cli = new CLI(usb);
+            cli.ConnectUsb();
+            cli.ConnectToGoldleaf();
+            bool result = cli.SendFile(TestUtils.GetResource("empty.txt"));
+            Assert.IsFalse(result, "Sending an empty(/invalid) txt must not succeed");
+        }
     }
 }
