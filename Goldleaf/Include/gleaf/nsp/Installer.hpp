a093115f826a540d01413a63a0a98109320693ac
XorTroll <xabofdez@outlook.es>
2019-01-13 00:05:22 +0100

    Big README update, some text changes

--- Goldleaf/Include/gleaf/nsp/Installer.hpp
+++ Goldleaf/Include/gleaf/nsp/Installer.hpp
@@ -1,9 +1,9 @@
 
 /*
 
-    Goldleaf - Nintendo Switch title manager homebrew
+    Goldleaf - Nintendo Switch homebrew multitool, for several purposes and with several features
 
-    Copyright 2018 - Goldleaf project, developed by XorTroll
+    Copyright 2018 - 2019 Goldleaf project, developed by XorTroll
     This project is under the terms of GPLv3 license: https://github.com/XorTroll/Goldleaf/blob/master/LICENSE
 
 */
